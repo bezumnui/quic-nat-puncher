b8acf8443773f21d480577728e53565d19d1afcd
bezumnui <bezumnui.mistikgt@gmail.com>
2026-05-16 17:53:22 +0400

    client test

--- client_test.py
+++ client_test.py
@@ -1,8 +1,7 @@
 ﻿import socket
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock.bind(('127.0.0.1', 12666))
 while True:
+    sock.sendto(b"hello", ('127.0.0.1', 12666))
     data, addr = sock.recvfrom(1024)
     print(data)
-    sock.sendto(data, addr)
