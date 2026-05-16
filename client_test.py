219276d2b72fc2163e8012452687907c1b3803b0
bezumnui <bezumnui.mistikgt@gmail.com>
2026-05-16 17:51:27 +0400

    test server

--- client_test.py
+++ client_test.py
@@ -1,7 +1,7 @@
 ﻿import socket
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock.bind(('127.0.0.1', 12345))
+sock.bind(('127.0.0.1', 12666))
 while True:
     data, addr = sock.recvfrom(1024)
     print(data)
